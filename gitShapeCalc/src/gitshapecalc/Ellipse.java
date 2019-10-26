a1d5cdadddd27da4d01815129c82fabd114c831a
MarZ <marwan.zeid00@gmail.com>
2019-10-26 17:54:51 -0400

    changed precision of perimeter and area values from 15 decimal places to 2 decimal places

--- gitShapeCalc/src/gitshapecalc/Ellipse.java
+++ gitShapeCalc/src/gitshapecalc/Ellipse.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package gitshapecalc;
-
+import java.math.*;
+import java.lang.*;
 /**
  *
  * @author S347391269
@@ -20,11 +21,13 @@ public class Ellipse extends Circle {
     
    @Override public double retPerimeter (){ // Override of the two base methods from the superclass
         double perimeter = Math.PI * (3 * (radius + radius2) - Math.sqrt((3 * radius + radius2) * (radius + 3 * radius2)));
-        return perimeter;
+        Double finaPeri = BigDecimal.valueOf(perimeter).setScale(3, RoundingMode.HALF_UP).doubleValue(); // Round double value to 2 decimal places
+        return finaPeri;
     }
     
    @Override public double retArea (){
         double area = Math.PI * radius * radius2;
-        return area;
+        Double finaArea = BigDecimal.valueOf(area).setScale(3, RoundingMode.HALF_UP).doubleValue();
+        return finaArea;
     }
 }
